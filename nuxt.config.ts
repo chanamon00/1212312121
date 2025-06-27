89918591010f3027cd18f121fe2d2b9bfa0b968c
sirRonnaphom <youremail@example.com>
2025-06-27 14:11:19 +0700

    Initial commit - Nuxt app with Vuetify

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  css: ['vuetify/styles'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG':false,
+    },
+  },
+  devtools: { enabled: true }
+})
