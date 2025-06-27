ebb7ac46814ae2c8cf8580d783a953ffbb347075
boonlong <kruboonlong@gmail.com>
2025-06-27 15:44:33 +0700

    add bootstrap cdn

--- app.vue
+++ app.vue
@@ -34,3 +34,20 @@
     <NuxtPage /> 
   </div>
 </template>
+
+<script setup>
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+    }
+  ],
+  script: [
+    {
+      src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+      defer: true
+    }
+  ]
+})
+</script>
