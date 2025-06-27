e1900f59eefb9f74014ca26530492af3feadf3a6
boonlong <kruboonlong@gmail.com>
2025-06-27 15:55:12 +0700

    update for

--- app.vue
+++ app.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-    </div>
-  </div>
-</nav>
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="/">Home</a>
+          <a class="nav-item nav-link" href="/page1">Page1</a>
+          <a class="nav-item nav-link" href="/page2">Page2</a>
+          <a class="nav-item nav-link" href="/page3">Page3</a>
+        </div>
+      </div>
+    </nav>
     <NuxtRouteAnnouncer />
-    
-    <NuxtPage /> 
+
+    <NuxtPage />
   </div>
 </template>
 
--- pages/page1.vue
+++ pages/page1.vue
@@ -1,3 +1,19 @@
 <template>
     คนที่1
+    <span v-for="i in 100"> {{ i }} </span> 
 </template>
+<script>
+export default {
+    data() {
+        return {
+            num1:0,
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    },
+}
+</script>
