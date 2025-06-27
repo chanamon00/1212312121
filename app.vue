2837a075d874d9ba7c25ad87320dfc0388b6375d
chanamon00 <chanamon20070503@gmail.com>
2025-06-27 16:09:54 +0700

    สร้าหน้าหลัก

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+  <v-container>
+  <h2> สมาชิกทีม </h2>
+  <ul>
+  <li><Nuxtlink to="/profile/ploy">ploy</Nuxtlink></li>
+  </ul>
+  <NuxtPage />
+  </v-container>
+  </v-app>
+    
+  
+
 </template>
